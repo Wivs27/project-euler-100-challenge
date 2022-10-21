0fee2dfb793602a44cd2f10afdd76b031219acdb
Wivs27 <88974160+Wivs27@users.noreply.github.com>
2022-10-21 16:53:09 +0100

    Added solution for problem 17

---
+++ Problem 17.py
@@ -0,0 +1,47 @@
+total = 11 #onethousand
+
+total += ((3*99)*9) #and
+
+total += (10 * 100) #onehundred
+total += (10 * 100) #twohundred
+total += (12 * 100) #threehundred
+total += (11 * 100) #fourhundred
+total += (11 * 100) #fivehundred
+total += (10 * 100) #sixhundred
+total += (12 * 100) #sevenhundred
+total += (12 * 100) #eighthundred
+total += (11 * 100) #ninehundred
+
+total += (3 * 90) #one
+total += (3 * 90) #two
+total += (5 * 90) #three
+total += (4 * 90) #four
+total += (4 * 90) #five
+total += (3 * 90) #six
+total += (5 * 90) #seven
+total += (5 * 90) #eight
+total += (4 * 90) #nine
+
+total += (6 * 100) #twenty
+total += (6 * 100) #thirty
+total += (5 * 100) #forty
+total += (5 * 100) #fifty
+total += (5 * 100) #sixty
+total += (7 * 100) #seventy
+total += (6 * 100) #eighty
+total += (6 * 100) #ninety
+
+total += (3 * 10) #ten
+total += (6 * 10) #eleven
+total += (6 * 10) #twelve
+total += (8 * 10) #thirteen
+total += (8 * 10) #fourteen
+total += (7 * 10) #fifteen
+total += (7 * 10) #sixteen
+total += (9 * 10) #seventeen
+total += (8 * 10) #eighteen
+total += (8 * 10) #nineteen
+
+
+
+print(total)
